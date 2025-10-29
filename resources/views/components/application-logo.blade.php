80d3503e290633eed32bf4a23e6725838fa21744
alwdev <69068330+alwdev@users.noreply.github.com>
2025-10-29 18:40:08 +0700

    <img src="{{ $siteSetting->logo     ? asset('storage/' . $siteSetting->logo)     : asset('images/default-logo.png') }}"     alt="Application Logo"     class="w-20 h-20 object-contain" />

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,2 +1,2 @@
-<img src="{{ asset('storage/' . $siteSetting->logo) }}" alt="Application Logo"
-    {{ $attributes->merge(['class' => 'w-20 h-20 fill-current text-gray-500']) }}>
+<img src="{{ $siteSetting->logo ? asset('storage/' . $siteSetting->logo) : asset('images/default-logo.png') }}"
+    alt="Application Logo" class="w-20 h-20 object-contain" />
